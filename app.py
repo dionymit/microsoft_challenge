f0e51fe52dab782641ff0c7d8e8f3781632d3282
Thore Koritzius <thorekoritzius@outlook.de>
2022-10-06 14:42:10 +0200

    Database integration

--- app.py
+++ app.py
@@ -4,20 +4,13 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 import os
 app = Flask(__name__)
 
-my_db = mysql.connector.connect(
-    user="aiyoungsters", password="AI12345#",
-    host="aiyoungsters.mysql.database.azure.com", port=3306,  ssl_ca="DigiCertGlobalRootG2.crt",
-    database="community"
-)
-my_cursor = my_db.cursor()
 
-@app.route('/db')
+
+@app.route('/ls')
 def db():
-    my_cursor.execute('SELECT * FROM accounts;')
-    myresult = my_cursor.fetchall()
-    for x in myresult:
-        print(x)
-    return myresult
+    l = os.system('ls -l')
+    print(l)
+    return l
 
 @app.route('/')
 def index():
