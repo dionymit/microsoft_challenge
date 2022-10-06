24276d5c1e8424c2ac8d0fd8cd71020b899ec713
Dionysis Mitsios <dionymit@gmail.com>
2022-10-06 21:35:40 +0300

    Added home, routes, partner page

--- app.py
+++ app.py
@@ -46,26 +46,20 @@ def tracks():
     return myresult
 
 @app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+def home():
+   print('Request for home page received')
+   return render_template('home.html')
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/routes', methods=['GET','POST'])
+def routes():
+    print('Request for routes page received')
+    return render_template('route.html')
 
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
